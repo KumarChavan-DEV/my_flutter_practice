154a71cffe242af62e0e21dadf035be86d52fff5
KumarNewjDev <kumar.chavan@thenewj.com>
2022-07-24 21:53:34 +0530

    Cart item was not displaying issue resolved

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -7,23 +7,23 @@ class CartModel {
 
   factory CartModel() => cartModel;
 
-  CatalogModel? _catalog = null;
+  CatalogModel? _catalog;
 
-  //Collection of IDs - store ids of each item
+//Collection of IDs - store ids of each item
   final List<int> _itemIds = [];
 
-  //Get Catalog
+//Get Catalog
   CatalogModel? get catalog => _catalog;
 
   set catalog(CatalogModel? newCatalog) {
     assert(newCatalog != null);
     _catalog = newCatalog!;
+    print("set catalog success");
   }
 
-// Get Items in the cart
-  List<Item?> get items => _itemIds.map((id) {
-        _catalog?.getById(id);
-      }).toList();
+// Get items in the cart
+  List<Item?> get items =>
+      _itemIds.map((id) => (_catalog?.getById(id))).toList();
 
 //Get Total Price
   num get totalPrice =>
